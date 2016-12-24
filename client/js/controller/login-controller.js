8a4194381b1475e480d69da259622dc3ccf490bf
xionghaitao@kingsoft.com <xionghaitao@kingsoft.com>
2016-12-24 13:04:55 +0800

    add server impl & in-memory-data persist leaved to be dig

--- client/js/controller/login-controller.js
+++ client/js/controller/login-controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
+import { Meteor } from 'meteor/meteor';
 export default function($scope, $meteor, $reactive, $timeout) {
 
    console.log('this is login controller...');
+
+   $reactive(this).attach($scope);
+   // 'ngInject';
 }
